c81aae4ae4855217dd20226c45047bae2d2466a7
Username <Email GitHub>
2025-02-06 13:23:24 +0700

    Initial commit with Next.js app and student ID

---
+++ src/app/Home/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to Next.js!</h1>
+        <p>Student ID: 66022815</p> {/* แทนที่ด้วยรหัสนิสิตของตัวเอง */}
+      </div>
+    )
+  }
+  
